e64db3eb3763f5d822a131049c1c736450c96f74
AndyPlus <andymore@live.com>
2022-06-24 16:12:16 +0800

    changed

--- Config/VsCodeExt.bat
+++ Config/VsCodeExt.bat
@@ -20,3 +20,57 @@ code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension timothymclane.react-redux-es6-snippets
 code --install-extension waderyan.gitblame
 code --install-extension xabikos.ReactSnippets
+
+
+==============================================
+#VSCode tips-and-tricks
+
+? edt
+Ctrl+R
+
+
+code -r .
+code --diff <file1> <file2>
+code --goto package.json:10:5
+
+
+Pressing the Alt key enables fast scrolling in the editor and Explorers. 	
+
+
+Ctrl+Shift+O  @:  	#Go to Symbol in File  
+Ctrl+T  #Go to Symbol in Workspace
+The Outline view in the File Explorer (default collapsed at the bottom) shows you the symbols 
+
+
+Undo cursor position#
+Keyboard Shortcut: Ctrl+U
+
+
+Code folding#
+Keyboard Shortcut: Ctrl+Shift+[ and Ctrl+Shift+]
+Fold All (Ctrl+K Ctrl+0) and Unfold All (Ctrl+K Ctrl+J).
+
+Select current line#
+Keyboard Shortcut: Ctrl+L
+
+
+Open Markdown preview#
+In a Markdown file, use
+
+Keyboard Shortcut: Ctrl+Shift+V
+
+
+
+IntelliSense#
+Ctrl+Space to trigger the Suggestions widget
+
+
+Peek#
+Select a symbol then type Alt+F12. Alternatively, you can use the context menu.
+
+
+Emmet syntax#
+Support for Emmet syntax.
+
+VS Snippets in VSCode
+
